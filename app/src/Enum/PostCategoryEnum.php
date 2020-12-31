d7fba3502e2b415f528c2a824e00f303e1557537
Tomislav <haxcontroler@gmail.com>
2020-12-31 01:57:18 +0100

    Updated UI and added a new Category

--- app/src/Enum/PostCategoryEnum.php
+++ app/src/Enum/PostCategoryEnum.php
@@ -9,17 +9,20 @@ class PostCategoryEnum
     public const RESIDENCY = 'residency';
     public const SUPPLIES = 'supplies';
     public const TRANSPORTATION = 'transportation';
+    public const OTHER = 'other';
 
     public const INTENT_CATEGORIES = [
         PostIntentEnum::LOOKING => [
             self::SUPPLIES,
             self::TRANSPORTATION,
             self::RESIDENCY,
+            self::OTHER,
         ],
         PostIntentEnum::OFFERING => [
             self::SUPPLIES,
             self::TRANSPORTATION,
             self::RESIDENCY,
+            self::OTHER,
         ]
     ];
 }
