53bef95644a7d09a7c4913467a812b2ca6696d6f
Mislav Jakopović <mislavjakopovic6@gmail.com>
2021-01-01 19:14:59 +0100

    One time cookie based post instructions

--- app/assets/js/pages/create.page.js
+++ app/assets/js/pages/create.page.js
@@ -1,3 +1,11 @@
+import Cookies from 'js-cookie'
+
 $(document).ready(function () {
-    $('#create-post-modal').modal()
+    if (Cookies.get('post-instructions') !== 'read') {
+        $('#create-post-modal').modal();
+    }
+});
+
+$('#create-post-modal').on('hidden.bs.modal', function (e) {
+    Cookies.set('post-instructions', 'read');
 });
